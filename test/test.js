9bb23b9be86cd2b4c5e90fc7394e98a8c013ce64
Onyemaechi Okafor <peerwaya@yahoo.com>
2016-06-05 12:33:59 +0100

    Adds firebase test

--- test/test.js
+++ test/test.js
@@ -10,9 +10,39 @@ test("Basic HTTP Tests - GET /{yourname*}", function(t) { // t
     server.inject(options, function(response) {
         t.equal(response.statusCode, 200);  //  Expect http response status code to be 200 ("Ok")
         t.equal(response.result.length, 12); // Expect result to be "Hello Timmy!" (12 chars long)
+        t.end();// t.end() callback is required to end the test in tape
+    });
+});
+
+test("Deny view of photo if unauthenticated /photo/{id*} ", function(t) {
+    var options = {
+        method: "GET",
+        url: "/photo/8795"
+    };
+    // server.inject lets you similate an http request
+    server.inject(options, function(response) {
+        t.equal(response.statusCode,401);  //  Expect http response status code to be 200 ("Ok")
+        t.equal(response.result.message,"Please log-in to see that"); // (Don't hard-code error messages)
+        t.end();
+    });
+});
+
+test("Login with fake credentials should return 401", function(t) {
+    var options = {
+        method: "POST",
+        url: "/login",
+        payload:{
+          "X-Verify-Credentials-Authorization":"value",
+          "X-Auth-Service-Provider":"https://api.digits.com/1.1/sdk/account.json"
+        }
+    };
+    // server.inject lets you similate an http request
+    server.inject(options, function(response) {
+        t.equal(response.statusCode,401);  //  Expect http response status code to be 200 ("Ok")
+        t.equal(response.result.message,"Unauthorized"); // (Don't hard-code error messages)
         server.stop(function(){
               console.log('done');
             });
-        t.end();// t.end() callback is required to end the test in tape
+        t.end();
     });
 });
